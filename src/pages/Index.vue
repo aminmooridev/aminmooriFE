da249368f0d0736d53365a4dc4283661e3980b07
Amin Moori <aminmoori.dev@gmail.com>
2020-08-23 16:32:03 +0430

    new quasar and some changes

---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page>
+    <div class="row window-height">
+      <div class="col-4 q-pa-lg">
+        <q-img class="full-height full-width rounded-borders shadow-6" src="/img/light.jpg"/>
+      </div>
+      <div class="col-8 flex flex-center">
+        <div class="row">
+          <div class="col-12">
+            <q-card flat bordered class=" my-card">
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
